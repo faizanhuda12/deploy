efc9e27d24c1fa4ec2af3d092533837f6bcb5082
Faizan <faizanhuda12@gmail.com>
2020-06-06 14:38:39 -0400

    deployment

---
+++ build/precache-manifest.98492086b7482c1a65c7f2e2a323714c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6175a53194c5c5c4f7b965c3734d0204",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7a8ec4e22fc8beb4015",
+    "url": "/static/css/main.3ca1f2ed.chunk.css"
+  },
+  {
+    "revision": "3b9b3598c83287bbb0d8",
+    "url": "/static/js/2.1d18d72b.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.1d18d72b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a8ec4e22fc8beb4015",
+    "url": "/static/js/main.f056203c.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
